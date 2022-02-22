f33abbf0b4a9a795a3cc91db7822018b2d63f9fd
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-22 20:08:06 +0100

    Inserito messaggio di benvenuto e numeri generati

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-// FUNZIONE PER GENERARE I NUMERI RANDOM
-function randomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+// MESSAGGIO DI BENVENUTO
+alert(`
+Ciao, oggi facciamo un gioco!
+
+Ti mostrerò 5 numeri, tu li dovrai tenere a mente, per poi reinserirli.
+Alla fine ti dirò se li hai indovinati tutti oppure no.
+
+Sei pronto/a? :)
+`);
+
+// ARRAY CHE CONTERRÀ I NUMERI RANDOM
+const arrayNumber = [];
+console.log(arrayNumber);
+
+for (let i = 0; i < 5; i++) {
+
+    // FUNZIONE PER GENERARE I NUMERI RANDOM
+    function randomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    // VARIABILE PER I NUMERI RANDOM
+    const numeriRandom = randomNumber(1, 99);
+    console.log(numeriRandom);
+
+    arrayNumber.push(numeriRandom);
 }
 
-// VARIABILE PER I NUMERI RANDOM
-const numeriRandom = randomNumber(1, 99);
-console.log(numeriRandom);
+// FUNZIONE CHE MOSTRA I NUMERI GENERATI CON 1,5 DI DELAY
+setTimeout(yourNumber, 1500);
+
+function yourNumber() {
+alert(`Questi sono i tuoi numeri:
+
+${arrayNumber}
+
+Tienili a mente per 30 secondi!
+`);
+}
