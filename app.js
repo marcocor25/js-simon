0c1986d6f400257d5e1a14e16db33335b3711685
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-22 19:18:06 +0100

    Generatore numeri random

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// TEST
+// FUNZIONE PER GENERARE I NUMERI RANDOM
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-console.log('Ciao a tutti!');
+// VARIABILE PER I NUMERI RANDOM
+const numeriRandom = randomNumber(1, 99);
+console.log(numeriRandom);
